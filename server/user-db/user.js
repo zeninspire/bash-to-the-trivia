ef451c78bbd9b8e92a60d8bb04bfb9003434ff29
Sevda Melek Anefi <astelvidax@Sevdas-MacBook-Pro.local>
2016-12-01 22:44:01 -0800

    updated ...

--- server/user-db/user.js
+++ server/user-db/user.js
@@ -1,17 +1,25 @@
 // CREATE A USER SCHEMA HERE
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-promised');
+var Promise = require('bluebird');
+
+
 var userSchema = mongoose.Schema({
   username: { type: String, required: true, index: {unique: true }},
   password: { type: String, required: true},
-});
-
-
+  channels: { type: Array },
+  avatarUrl: { type: String },
+  age: { type: Number, min: 12, max: 65 },
+  created: Date,
+  updated: { type: Date, default: Date.now }
 
 
 
 var User = mongoose.model('User', userSchema);
 
-
-
-
-exports.module = User;
+userSchema.pre('save', function(next) {
+  return bcrypt.hash(this.password, null).then(function(hash) {
+    this.password = hash;
+  })
+  next();
+})
