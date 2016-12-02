79e3ff16d9e6cf4b9067da998a2529d39370fe24
zeninspire <zeninspire@gmail.com>
2016-12-01 17:09:51 -0800

    child views and routing created

--- client/app/auth/auth-controller.js
+++ client/app/auth/auth-controller.js
@@ -1,7 +1,13 @@
 angular.module('app.auth', [])
 
 
-.controller('AuthController', function() {
+.controller('AuthController', function($scope) {
+
+  $scope.signInData=''
+  $scope.signIn=function(){
+    console.log('test ng-click')
+    //TODO: SEND signin info to server to check if user exists; if so, check if passwords is correct.
+  }
 
 
 });
