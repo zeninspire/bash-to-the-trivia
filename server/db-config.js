06c0058e7d7d66c69225eebb7f521eea42b5213e
zeninspire <zeninspire@gmail.com>
2016-12-12 12:13:51 -0800

    new mlab

--- server/db-config.js
+++ server/db-config.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose');
 
-var currentDB = process.env.MONGOLAB_PUCE_URI || 'mongodb://localhost/users';
+var currentDB = process.env.MONGODB_URI || 'mongodb://localhost/users';
 
-// 'mongodb://zeninspire:bash2thet@ds127948.mlab.com:27948/heroku_fzshsnrq'
-
-// 'mongodb://heroku_kktw8x15:5d30s1qij57q26i6sttj2hlc1v@ds133378.mlab.com:33378/heroku_kktw8x15'
+// 'mongodb://heroku_1sdtg5p6:f79acqoi6othgbc3sk0qbponk8@ds133358.mlab.com:33358/heroku_1sdtg5p6'
 
 //set the connect URL to currentDB;
 mongoose.connect(currentDB);
